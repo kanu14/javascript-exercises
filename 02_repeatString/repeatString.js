8e2149a5847256d24e0c8f1489f5d1661416cbd7
kanu14 <mparkeh14@gmail.com>
2024-11-15 23:30:33 -0800

    updated problems 1-3

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,4 +1,16 @@
-const repeatString = function() {
+const repeatString = function(str, num) {
+    let string = "";
+
+    if (num > 0) {
+        for (let i = 0; i < num; i++) {
+            string += str;
+        }
+        return string;
+    } else if ( num == 0) {
+        return (string ="");
+    } else {
+        return 'ERROR';
+    }
 
 };
 
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    let string = str.split("");
+    let reverse = string.reverse();
+    let new_str = reverse.join("");
+    return new_str;
 };
 
 // Do not edit below this line
