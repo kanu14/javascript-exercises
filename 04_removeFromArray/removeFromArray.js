f5d498c9078894505aa67167bc2dc1a1e067fcf0
kanu14 <mparkeh14@gmail.com>
2024-11-22 23:20:23 -0800

    updated up to problem 5

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,22 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...remove) {
+    // if (!Array.isArray(remove)) {
+    //     remove = [remove];
+    // }
+    // let deleteItem = [remove];
+
+    for (let i = 0; i < remove.length; i++) {
+        // iterate backwards so that the loop doesnt impact the array order
+        for(let k = array.length - 1; k >= 0; k--) {
+            
+            if ( array[k] === remove[i]) {
+                array.splice(k, 1);
+                
+            }   
+        }
+    }
+    return array;
 };
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
-
+const sumAll = function(a, b) {
+    let total = 0;
+    if (a < 0 || b < 0) {
+        return ('ERROR');
+    }
+    if (Number.isInteger(a) && Number.isInteger(b)) {
+        let diff;
+        if ((b - a) > 0) {
+            diff = b - a;
+        } else {
+            diff = a - b;
+        }
+        for (let i = 0; i <= diff; i++) {
+            total = total + i + Math.min(a , b);
+            
+        }
+        return total;
+    } else {
+        return ('ERROR');
+    }
 };
 
 // Do not edit below this line
